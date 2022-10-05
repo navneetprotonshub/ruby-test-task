7124208db896f9d87709c534fe5d9bf44f2bf83f
developer <developer@gmail.com>
2022-10-05 12:29:19 +0530

    rspec problem solved

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,25 @@
 class Serializer
+  class << self
+    attr_reader :elements
+
+    def attribute(element, &block)
+      @elements ||= {}
+      @elements[element] = block
+    end
+  end
+
+  attr_reader :object
+
+  def initialize(object)
+    @object = object
+  end
+
+  def serialize
+    serialized_data = {}
+    self.class.elements.each do |element, block|
+      serialized_data[element] = object.send(element)
+      serialized_data[element] = instance_eval(&block) unless block.nil?
+    end
+    serialized_data.compact
+  end
 end
